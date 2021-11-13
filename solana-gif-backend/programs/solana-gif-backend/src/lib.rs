cb8187b8f490aec706fba308a17fd66c2f38ebf0
Saurabh Srivastava <vasudeveloper001@gmail.com>
2021-11-13 14:37:17 +0530

    Local development v1 done - before deployment to devnet

--- solana-gif-backend/programs/solana-gif-backend/src/lib.rs
+++ solana-gif-backend/programs/solana-gif-backend/src/lib.rs
@@ -5,10 +5,60 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod solana_gif_backend {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+        // Get a reference to the account
+        let base_account = &mut ctx.accounts.base_account;  // We do &mut to get a "mutable reference" to base_account. When we do this it actually gives us the power to make changes to base_account. Otherwise, we'd simply be working w/ a "local copy" of base_account.
+        // Initialise total_gifs
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+        };
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff <'info> {
+#[account(init, payer = user, space = 9000)]
+// All we're doing here is telling Solana how we want to initialize BaseAccount
+// init will tell Solana to create a new account owned by our current program.
+// payer = user tells our program who's paying for the account to be created. In this case, it's the user calling the function.
+// We then say space = 9000 which will allocate 9000 bytes of space for our account. You can change this # if you wanted, but, 9000 bytes is enough for the program we'll be building here!
+
+pub base_account: Account <'info, BaseAccount >,
+#[account( mut)]
+pub user: Signer<'info>,
+pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub gif_list: Vec<ItemStruct>,
+} // Basically, it tells our program what kinda of account it can make and what to hold inside of it. So, here, BaseAccount holds one thing and it's an integer named total_gifs.
--- solana-gif-backend/tests/solana-gif-backend.js
+++ solana-gif-backend/tests/solana-gif-backend.js
@@ -1,14 +1,53 @@
 const anchor = require('@project-serum/anchor');
 
-describe('solana-gif-backend', () => {
+const { SystemProgram } = anchor.web3; // Need the system program, will talk about this soon.
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+const main = async () => {
+  console.log('Starting test...');
+  const provider = anchor.Provider.env(); // gets this data from solana config get
+  anchor.setProvider(provider); // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const program = anchor.workspace.SolanaGifBackend; // given to us by Anchor that will automatically compile our code in lib.rs and get it deployed locally on a local validator.
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate(); // we are doing this because we need to create some credentials for the BaseAccount we're creating.
+  // Call start_stuff_off, pass it the params it needs!
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  }); // call our function we made by doing program.rpc.startStuffOff() and we await it which will wait for our local validator to "mine" the instruction.
+  console.log('Your transaction signature', tx);
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString()); // Here we actually retrieve the account we created and then access totalGifs
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.SolanaGifBackend;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
-});
+  await program.rpc.addGif(
+    'https://c.tenor.com/tk1XbNj4Y_wAAAAd/squid-game.gif',
+    {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    }
+  );
+  // Get the account again to see what changed.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString());
+
+  // Access gif_list on the account!
+  console.log('👀 GIF List', account.gifList);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
